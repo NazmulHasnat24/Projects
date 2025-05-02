c6578539883a6b57b7bc1ebc3f6b0eadfded9d39
Nazmul Hasnat <nhasnat24@gmail.com>
2025-05-03 00:17:49 +0600

    Student Registarion

--- 5.Student_Registration.dart
+++ 5.Student_Registration.dart
@@ -1,12 +1,22 @@
 main(){
 
+//object creation
+
 StudentInfo info = StudentInfo("Nazmul", 222006, "xyz", "A+");
 StudentInfo info2 = StudentInfo("Hasnat", 222007, "xyz", "A+");
+
+// calling object referrence
+
 info.display();
 info2.display();
+
+//updating grade for 2nd object
+
 info2.GradeUpdate("A", 222007); 
 
 }
+
+// class
 
 class StudentInfo{
 
@@ -15,11 +25,17 @@ int ? Id;
 String ? Section;
 String ? Grade;
 
+//constructor with perameter
+
 StudentInfo(this.Name, this.Id, this.Section, this.Grade){
 
-
+print("-----------Student Information----------");
+print("\n");
 
 }
+
+
+//display method for printing 
 
 void display(){
 
@@ -29,14 +45,19 @@ void display(){
   print("Student Grade : $Grade");
   print("\n");
 
+
 }
+
+//updating method
 
 void GradeUpdate(String Grade, int Id){
 
 this.Grade = Grade;
 this.Id = Id;
 
-print("Updated Grade of student Id $Id : $Grade"); 
+print("Updated Grade of student Id $Id : $Grade");
+
+
 }
 
 }
