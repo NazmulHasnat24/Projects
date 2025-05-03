838c726d14d22d613a56ef06b08179021f87f1f0
Nazmul Hasnat <nhasnat24@gmail.com>
2025-05-03 12:03:10 +0600

    Main Class

---
+++ 6.UnitConverter_App/Length.dart
@@ -0,0 +1,17 @@
+class Length {
+
+double ? Meter;
+double ? Kilometer;
+
+Length(this.Meter){
+
+}
+
+void display(){
+
+Kilometer = Meter! / 1000;
+print("$Meter Meter >> Kilometer : $Kilometer");
+
+}
+
+}
---
+++ 6.UnitConverter_App/main.dart
@@ -0,0 +1,7 @@
+import 'Length.dart';
+void main(){
+
+Length length = Length(2000);
+length.display();
+
+}
