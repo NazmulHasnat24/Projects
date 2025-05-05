e47ac1d163086f4398aae65c5a9e61c7e641d41c
Nazmul Hasnat <nhasnat24@gmail.com>
2025-05-06 00:26:39 +0600

    Unit Converter App

--- 6.UnitConverter_App/main.dart
+++ 6.UnitConverter_App/main.dart
@@ -4,57 +4,104 @@ import 'Temperature.dart';
 import 'Weight.dart';
 import 'Speed.dart';
 import 'Volume.dart';
+
+
 void main(){
+
+print("\n");
+print("(1)Length Calculation");
+print("(2)Temperature Calculation");
+print("(3)Weight Calculation");
+print("(4)Speed Calculation");
+print("(5)Volume Calculation");
+print("\n\n");
+
+print("------ Press Button ------");
+
+
+int ? Button = int.tryParse(stdin.readLineSync()!);
+
+
+switch(Button){
+
+    case 1:
 
 // Length
 
-print("Enter a value to convert into kilometer :");
+    print("Enter a value to convert into kilometer :");
 
-double ? Meter = double.tryParse(stdin.readLineSync()!);
+    double ? Meter = double.tryParse(stdin.readLineSync()!);
 
-Length length = Length(Meter);
+    Length length = Length(Meter);
 
-length.display();
+    length.display();
+
+break;
+
+    case 2:
 
 // Temperature
 
-print("Enter a value to convert into Farenheit :");
+    print("Enter a value to convert into Farenheit :");
 
-double ? Celcius = double.tryParse(stdin.readLineSync()!);
+    double ? Celcius = double.tryParse(stdin.readLineSync()!);
 
-Temperature temperature = Temperature(Celcius);
+    Temperature temperature = Temperature(Celcius);
 
-temperature.display();
+    temperature.display();
+
+break;
+
+    case 3 :
 
 //Weight
 
-print("Enter a value to convert into pounds :");
+    print("Enter a value to convert into pounds :");
 
-double ? Kilogram = double.tryParse(stdin.readLineSync()!);
+    double ? Kilogram = double.tryParse(stdin.readLineSync()!);
 
-Weight weight = new Weight(Kilogram);
+    Weight weight = new Weight(Kilogram);
 
-weight.display();
+    weight.display();
+
+
+break;
+
+    case 4 :
 
 //Speed
 
-print("Enter a value to convert into miles :");
+    print("Enter a value to convert into miles :");
 
-double ? KiloMeter = double.tryParse(stdin.readLineSync()!);
+    double ? KiloMeter = double.tryParse(stdin.readLineSync()!);
 
-Speed speed = Speed(KiloMeter);
+    Speed speed = Speed(KiloMeter);
 
-speed.display();
+    speed.display();
+
+break;
+
+
+    case 5 :
 
 //Volume
 
-print("Enter a value to convert into Gallons :");
+    print("Enter a value to convert into Gallons :");
 
-double ? Liter = double.tryParse(stdin.readLineSync()!);
+    double ? Liter = double.tryParse(stdin.readLineSync()!);
 
-Volume volume = Volume(Liter);
+    Volume volume = Volume(Liter);
 
-volume.display();
+    volume.display();
+
+break;
+    
+    default :
+
+        print("Invalid Input!!!"); 
+
+
+ }
 
 
 }
